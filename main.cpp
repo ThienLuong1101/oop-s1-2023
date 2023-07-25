c410f26d1c6f3be53c785fb70f566362125149aa
luongthien <luongthien1236@gmail.com>
2023-07-25 11:24:35 +0930

    Add main.cpp and count.cpp files for workshop1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 extern int count(int[], int);
 
 int main() {
-    int array[5] = {4,5,6,-7,8};
+    int array[5] = {4,5,6,7,8};
     std::cout << "The number is: " << count(array, 5) << std::endl;
     return 0;
 }
