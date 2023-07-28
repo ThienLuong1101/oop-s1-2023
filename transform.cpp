571dd412ac1b28bfcd5d3852903108009b1658c8
luongthien <luongthien1236@gmail.com>
2023-07-28 15:17:34 +0930

    workshop2

---
+++ transform.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+void transform(int base_10) {
+    int digits[32]; // Assuming a 32-bit integer, adjust the size as needed
+    int position = 0;
+
+    while (base_10 != 0)
+    {
+        digits[position] = base_10 % 2;
+        base_10 = base_10 / 2;
+        position++;
+    }
+
+    for (int i = position - 1; i >= 0; i--)
+    {
+        std::cout << digits[i];
+    }
+    std::cout << std::endl;
+
+}
