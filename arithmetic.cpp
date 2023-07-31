67b6dfdab9ad4252dc6fc9c9aa912a215ea92f2b
Uncle10 <117525299+ThienLuong1101@users.noreply.github.com>
2023-07-31 16:26:48 +0930

    Update arithmetic.cpp

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,30 +1,65 @@
-#include<iostream>
-using namespace std;
-using big = unsigned long int;
+#include <iostream>
+#include <cmath>
 
-extern int base_10_to_2(unsigned long int base_2);
-extern void base_2_to_10(int base_10);
-extern int add(int num1, int num2);
-extern int subtract(int num1, int num2);
+// Function to convert a base-10 number to base-2 and print it
+void base_10_to_2(int base_10) {
+    int digits[32]; // Assuming a 32-bit integer, adjust the size as needed
+    int position = 0;
+
+    while (base_10 != 0) {
+        digits[position] = base_10 % 2;
+        base_10 /= 2;
+        position++;
+    }
+
+    for (int i = position - 1; i >= 0; i--) {
+        std::cout << digits[i];
+    }
+    std::cout << std::endl;
+}
+
+// Function to convert a base-2 number to base-10
+int base_2_to_10(unsigned long int base_2) {
+    int num = 0;
+    int position = 0;
+
+    while (base_2 > 0) {
+        int last = base_2 % 10;
+        num += last * pow(2, position);
+        position++;
+        base_2 /= 10;
+    }
+
+    return num;
+}
+
+int add(int num1, int num2) {
+    return num1 + num2;
+}
+
+int subtract(int num1, int num2) {
+    return num1 - num2;
+}
+
 int main() {
-    big num1;
-    big num2;
+    unsigned long int num1, num2;
 
-    cout << "Enter number1 (base2): ";
-    cin >> num1;
+    std::cout << "Enter number1 (base2): ";
+    std::cin >> num1;
 
-    cout << "Enter number2 (base2): ";
-    cin >> num2;
+    std::cout << "Enter number2 (base2): ";
+    std::cin >> num2;
 
-    int ans1 = base_10_to_2(num1);
-    int ans2 =  base_10_to_2(num2);
+    int ans1 = base_2_to_10(num1);
+    int ans2 = base_2_to_10(num2);
 
     int sum = add(ans1, ans2);
     int sub = subtract(ans1, ans2);
-    cout << "sum (base2): ";
-    base_2_to_10(sum);
-    cout << "subtract (base2): ";
-    base_2_to_10(sub);
-    
+
+    std::cout << "sum (base2): ";
+    base_10_to_2(sum);
+    std::cout << "subtract (base2): ";
+    base_10_to_2(sub);
+
     return 0;
 }
